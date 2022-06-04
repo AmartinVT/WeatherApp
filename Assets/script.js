0b67fc005ae7b29f6af8b559e5497719aa46d612
VT_AOE_91 <amartin80108@gmail.com>
2022-06-04 16:52:06 -0400

    created unix variables for today + 5 days. NOT working properly in queryURL for API

--- Assets/script.js
+++ Assets/script.js
@@ -6,11 +6,17 @@ var APIKey = "4078811ac12257abe5c1a8aa51a3bfd6"; //API Key to access OpenWeather
 let i = 0; //Variable initialization for search save indexing
 
 let a_date = moment().format("MMM Do"); //Today
+let a_date_unix = moment().unix(); //Today (Unix)
 let b_date = moment().add(1, 'days').format("MMM Do"); //Today + 1
+let b_date_unix = moment().add(1, 'days').unix(); //Today + 1 (Unix)
 let c_date = moment().add(2, 'days').format("MMM Do"); //Today + 2
+let c_date_unix = moment().add(2, 'days').unix(); //Today + 2 (Unix)
 let d_date = moment().add(3, 'days').format("MMM Do"); //Today + 3
+let d_date_unix = moment().add(3, 'days').unix(); //Today + 3 (Unix)
 let e_date = moment().add(4, 'days').format("MMM Do"); //Today + 4
+let e_date_unix = moment().add(4, 'days').unix(); //Today + 4 (Unix)
 let f_date = moment().add(5, 'days').format("MMM Do"); //Today + 5
+let f_date_unix = moment().add(5, 'days').unix(); //Today + 5 (Unix)
 
 // Search button controls
 searchBtnEl.on('click', function() { //On press of save button, saves input to local storage 
@@ -20,28 +26,26 @@ searchBtnEl.on('click', function() { //On press of save button, saves input to l
     console.log(`Saved Search :  ${city} `); //Confirms the searched city was saved
     
     //OpenWeatherAPI call for today's date
-    var queryURL = "http://api.openweathermap.org/data/2.5/weather?q=" + city + "&appid=" + APIKey + "&units=imperial"; //Variable for URL for the API with user input city name and API key
+    var queryURL = "http://api.openweathermap.org/data/2.5/weather?q=" + city + "&appid=" + APIKey + "&units=imperial" + "&dt=" + a_date_unix; //Variable for URL for the API with user input city name and API key
     fetch(queryURL) //API call
     .then(function (response){ //Response of the API call
         return response.json()
     })
     .then(function(data){ //Printing of the API call to the console
-        a_dt = data.dt;
         a_feelsLike = data.main.feels_like;
         a_weatherIcon = data.weather[0].icon;
         a_wind = data.wind.speed;
         a_humidity = data.main.humidity;
         console.log(data);
         console.log(a_date);
-        console.log(a_dt);
+        console.log(a_date_unix);   
         console.log(a_feelsLike);
         console.log(a_weatherIcon);
         console.log(a_wind);
         console.log(a_humidity);
 
         //OpenWeatherAPI call for today's date + 1
-        var b_dt = a_dt + 1;
-        var queryURL = "http://api.openweathermap.org/data/2.5/weather?q=" + city + "&appid=" + APIKey + "&units=imperial" + "&dt=" + b_dt; //Variable for URL for the API with user input city name and API key
+        var queryURL = "http://api.openweathermap.org/data/2.5/weather?q=" + city + "&appid=" + APIKey + "&units=imperial" + "&dt=" + b_date_unix; //Variable for URL for the API with user input city name and API key
         fetch(queryURL) //API call
         .then(function (response){ //Response of the API call
             return response.json()
@@ -53,15 +57,14 @@ searchBtnEl.on('click', function() { //On press of save button, saves input to l
             b_humidity = data.main.humidity;
             console.log(data);
             console.log(b_date);
-            console.log(b_dt);
+            console.log(b_date_unix); 
             console.log(b_feelsLike);
             console.log(b_weatherIcon);
             console.log(b_wind);
             console.log(b_humidity);
         
             //OpenWeatherAPI call for today's date + 2
-            var c_dt = b_dt + 1;
-            var queryURL = "http://api.openweathermap.org/data/2.5/weather?q=" + city + "&appid=" + APIKey + "&units=imperial" + "&dt=" + c_dt; //Variable for URL for the API with user input city name and API key
+            var queryURL = "http://api.openweathermap.org/data/2.5/weather?q=" + city + "&appid=" + APIKey + "&units=imperial" + "&dt=" + c_date_unix; //Variable for URL for the API with user input city name and API key
             fetch(queryURL) //API call
             .then(function (response){ //Response of the API call
                 return response.json()
@@ -73,15 +76,14 @@ searchBtnEl.on('click', function() { //On press of save button, saves input to l
                 c_humidity = data.main.humidity;
                 console.log(data);
                 console.log(c_date);
-                console.log(c_dt);
+                console.log(c_date_unix); 
                 console.log(c_feelsLike);
                 console.log(c_weatherIcon);
                 console.log(c_wind);
                 console.log(c_humidity);
                 
                 //OpenWeatherAPI call for today's date + 3
-                var d_dt = c_dt + 1;
-                var queryURL = "http://api.openweathermap.org/data/2.5/weather?q=" + city + "&appid=" + APIKey + "&units=imperial" + "&dt=" + d_dt; //Variable for URL for the API with user input city name and API key
+                var queryURL = "http://api.openweathermap.org/data/2.5/weather?q=" + city + "&appid=" + APIKey + "&units=imperial" + "&dt=" + d_date_unix; //Variable for URL for the API with user input city name and API key
                 fetch(queryURL) //API call
                 .then(function (response){ //Response of the API call
                     return response.json()
@@ -93,15 +95,14 @@ searchBtnEl.on('click', function() { //On press of save button, saves input to l
                     d_humidity = data.main.humidity;
                     console.log(data);
                     console.log(d_date);
-                    console.log(d_dt);
+                    console.log(d_date_unix); 
                     console.log(d_feelsLike);
                     console.log(d_weatherIcon);
                     console.log(d_wind);
                     console.log(d_humidity);
                 
                     //OpenWeatherAPI call for today's date + 4
-                    var e_dt = d_dt + 1;
-                    var queryURL = "http://api.openweathermap.org/data/2.5/weather?q=" + city + "&appid=" + APIKey + "&units=imperial" + "&dt=" + e_dt; //Variable for URL for the API with user input city name and API key
+                    var queryURL = "http://api.openweathermap.org/data/2.5/weather?q=" + city + "&appid=" + APIKey + "&units=imperial" + "&dt=" + e_date_unix; //Variable for URL for the API with user input city name and API key
                     fetch(queryURL) //API call
                     .then(function (response){ //Response of the API call
                         return response.json()
@@ -113,15 +114,14 @@ searchBtnEl.on('click', function() { //On press of save button, saves input to l
                         e_humidity = data.main.humidity;
                         console.log(data);
                         console.log(e_date);
-                        console.log(e_dt);
+                        console.log(e_date_unix); 
                         console.log(e_feelsLike);
                         console.log(e_weatherIcon);
                         console.log(e_wind);
                         console.log(e_humidity);
                     
                         //OpenWeatherAPI call for today's date + 5
-                        var f_dt = e_dt + 1;
-                        var queryURL = "http://api.openweathermap.org/data/2.5/weather?q=" + city + "&appid=" + APIKey + "&units=imperial" + "&dt=" + f_dt; //Variable for URL for the API with user input city name and API key
+                        var queryURL = "http://api.openweathermap.org/data/2.5/weather?q=" + city + "&appid=" + APIKey + "&units=imperial" + "&dt=" + f_date_unix; //Variable for URL for the API with user input city name and API key
                         fetch(queryURL) //API call
                         .then(function (response){ //Response of the API call
                             return response.json()
@@ -133,7 +133,7 @@ searchBtnEl.on('click', function() { //On press of save button, saves input to l
                             f_humidity = data.main.humidity;
                             console.log(data);
                             console.log(f_date);
-                            console.log(f_dt);
+                            console.log(a_date_unix);                             
                             console.log(f_feelsLike);
                             console.log(f_weatherIcon);
                             console.log(f_wind);
