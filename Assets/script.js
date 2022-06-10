fe44957fa37047b682ebc6d27501a68cbaad2758
VT_AOE_91 <amartin80108@gmail.com>
2022-06-10 17:22:57 -0400

    Updated javascript to update HTML for all dates.

--- Assets/script.js
+++ Assets/script.js
@@ -109,16 +109,88 @@ searchBtnEl.on('click', function() { //On press of save button, saves input to l
     })
     .then(function(data){ //Printing of the API call to the console
         console.log(data)
+        
+        //Day + 0 Data
         a_feelsLike = data.daily[0].feels_like.day;
         a_weatherIcon = data.daily[0].weather[0].icon;
         a_wind = data.daily[0].wind_speed;
         a_humidity = data.daily[0].humidity;
         a_uv = data.daily[0].uvi;
+
+        //Day + 1 Data
+        b_feelsLike = data.daily[1].feels_like.day;
+        b_weatherIcon = data.daily[1].weather[0].icon;
+        b_wind = data.daily[1].wind_speed;
+        b_humidity = data.daily[1].humidity;
+
+        //Day + 2 Data
+        c_feelsLike = data.daily[2].feels_like.day;
+        c_weatherIcon = data.daily[2].weather[0].icon;
+        c_wind = data.daily[2].wind_speed;
+        c_humidity = data.daily[2].humidity;
+
+        //Day + 3 Data
+        d_feelsLike = data.daily[3].feels_like.day;
+        d_weatherIcon = data.daily[3].weather[0].icon;
+        d_wind = data.daily[3].wind_speed;
+        d_humidity = data.daily[3].humidity;
+
+        //Day + 4 Data
+        e_feelsLike = data.daily[4].feels_like.day;
+        e_weatherIcon = data.daily[4].weather[0].icon;
+        e_wind = data.daily[4].wind_speed;
+        e_humidity = data.daily[4].humidity;
+
+        //Day + 5 Data
+        f_feelsLike = data.daily[5].feels_like.day;
+        f_weatherIcon = data.daily[5].weather[0].icon;
+        f_wind = data.daily[5].wind_speed;
+        f_humidity = data.daily[5].humidity;
+
+        // Updating HTML
+
+        //Day + 0 HTML Update
         document.getElementById("date0date").innerHTML = "TODAY: " + a_date;
         document.getElementById("date0icon").src = `http://openweathermap.org/img/wn/${a_weatherIcon}@2x.png`;
         document.getElementById("date0temp").innerHTML = "Feels Like: " + a_feelsLike + " F";
         document.getElementById("date0wind").innerHTML = "Wind Speed: " + a_wind + " MPH";
         document.getElementById("date0humid").innerHTML = "Humidity: " + a_humidity + " %";
+
+        //Day + 1 HTML Update
+        document.getElementById("date1date").innerHTML = "TOMORROW: " + b_date;
+        document.getElementById("date1icon").src = `http://openweathermap.org/img/wn/${b_weatherIcon}@2x.png`;
+        document.getElementById("date1temp").innerHTML = "Feels Like: " + b_feelsLike + " F";
+        document.getElementById("date1wind").innerHTML = "Wind Speed: " + b_wind + " MPH";
+        document.getElementById("date1humid").innerHTML = "Humidity: " + b_humidity + " %";
+
+        //Day + 2 HTML Update
+        document.getElementById("date2date").innerHTML = c_date;
+        document.getElementById("date2icon").src = `http://openweathermap.org/img/wn/${c_weatherIcon}@2x.png`;
+        document.getElementById("date2temp").innerHTML = "Feels Like: " + c_feelsLike + " F";
+        document.getElementById("date2wind").innerHTML = "Wind Speed: " + c_wind + " MPH";
+        document.getElementById("date2humid").innerHTML = "Humidity: " + c_humidity + " %";         
+
+        //Day + 3 HTML Update
+        document.getElementById("date3date").innerHTML = d_date;
+        document.getElementById("date3icon").src = `http://openweathermap.org/img/wn/${d_weatherIcon}@2x.png`;
+        document.getElementById("date3temp").innerHTML = "Feels Like: " + d_feelsLike + " F";
+        document.getElementById("date3wind").innerHTML = "Wind Speed: " + d_wind + " MPH";
+        document.getElementById("date3humid").innerHTML = "Humidity: " + d_humidity + " %";
+
+        //Day + 4 HTML Update
+        document.getElementById("date4date").innerHTML = e_date;
+        document.getElementById("date4icon").src = `http://openweathermap.org/img/wn/${e_weatherIcon}@2x.png`;
+        document.getElementById("date4temp").innerHTML = "Feels Like: " + e_feelsLike + " F";
+        document.getElementById("date4wind").innerHTML = "Wind Speed: " + e_wind + " MPH";
+        document.getElementById("date4humid").innerHTML = "Humidity: " + e_humidity + " %";
+
+        //Day + 5 HTML Update
+        document.getElementById("date5date").innerHTML = f_date;
+        document.getElementById("date5icon").src = `http://openweathermap.org/img/wn/${f_weatherIcon}@2x.png`;
+        document.getElementById("date5temp").innerHTML = "Feels Like: " + f_feelsLike + " F";
+        document.getElementById("date5wind").innerHTML = "Wind Speed: " + f_wind + " MPH";
+        document.getElementById("date5humid").innerHTML = "Humidity: " + f_humidity + " %";           
+  
     })
 
     if (i < 4) {
